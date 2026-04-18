2389d44b288b7d3d1119c427137fb46fd51643dc
Indragunwan <158044686+Indragunwan@users.noreply.github.com>
2026-04-18 16:57:33 +0700

    update db py
    Use NullPool for connection limits in Serverless/Vercel.

--- backend/database.py
+++ backend/database.py
@@ -2,6 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import NullPool
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./feeds.db")
 
@@ -13,7 +14,8 @@ if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
         SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
     )
 else:
-    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+    # Menggunakan NullPool untuk mengatasi batas koneksi di Serverless / Vercel
+    engine = create_engine(SQLALCHEMY_DATABASE_URL, poolclass=NullPool)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
